869829ffa3f2c61ce71b052171c8201f2105f1ff
t-murata <th.murata@syslg.co.jp>
2025-11-27 16:09:48 +0900

    add:Case10.java 勤怠登録機能の出退勤時間打刻機能をテストするテストコード追加
    WebElementを利用してログイン後に、勤怠登録機能の出退勤時間打刻機能が期待通り動作するかどうか確認するテストを作成
    
    期待値は以下の通り
    「出勤」ボタンを押下し、テスト実施時刻が出勤時間に登録される
    「退勤」ボタンを押下し、テスト実施時刻が退勤時間に登録される

--- test_code_creation_muratatomohiro/src/test/java/jp/co/sss/lms/ct/f04_attendance/Case10.java
+++ test_code_creation_muratatomohiro/src/test/java/jp/co/sss/lms/ct/f04_attendance/Case10.java
@@ -1,6 +1,10 @@
 package jp.co.sss.lms.ct.f04_attendance;
 
 import static jp.co.sss.lms.ct.util.WebDriverUtils.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
@@ -9,6 +13,11 @@ import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoAlertPresentException;
+import org.openqa.selenium.WebElement;
+
+import jp.co.sss.lms.ct.util.WebDriverUtils;
 
 /**
  * 結合テスト 勤怠管理機能
@@ -35,35 +44,134 @@ public class Case10 {
 	@Order(1)
 	@DisplayName("テスト01 トップページURLでアクセス")
 	void test01() {
-		// TODO ここに追加
+		goTo("http://localhost:8080/lms/");
+
+		getEvidence(new Object() {
+		}, "ケース10_No.01");
+
+		assertEquals(webDriver.getTitle(), "ログイン | LMS");
+		assertEquals(webDriver.findElement(By.className("btn")).getAttribute("value"), "ログイン");
+		assertEquals(webDriver.getCurrentUrl(), "http://localhost:8080/lms/");
 	}
 
 	@Test
 	@Order(2)
 	@DisplayName("テスト02 初回ログイン済みの受講生ユーザーでログイン")
 	void test02() {
-		// TODO ここに追加
+		// ログインに必要な要素の取得
+		WebElement userID = WebDriverUtils.webDriver.findElement(By.id("loginId"));
+		WebElement password = WebDriverUtils.webDriver.findElement(By.id("password"));
+		WebElement loginButton = WebDriverUtils.webDriver.findElement(By.cssSelector("[value='ログイン']"));
+		userID.clear();
+		userID.sendKeys("StudentAA01");
+		password.clear();
+		password.sendKeys("Password01");
+
+		getEvidence(new Object() {
+		}, "ケース10_No.02_1");
+
+		loginButton.click();
+
+		visibilityTimeout(By.className("active"), 10);
+
+		assertEquals(webDriver.getTitle(), "コース詳細 | LMS");
+
+		getEvidence(new Object() {
+		}, "ケース10_No.02_2");
 	}
 
 	@Test
 	@Order(3)
 	@DisplayName("テスト03 上部メニューの「勤怠」リンクから勤怠管理画面に遷移")
-	void test03() {
-		// TODO ここに追加
+	void test03() throws InterruptedException {
+		WebElement attendanceLink = webDriver.findElement(By.linkText("勤怠"));
+
+		getEvidence(new Object() {
+		}, "ケース10_No.03_1");
+
+		attendanceLink.click();
+
+		try {
+			// アラートが出てきた場合はOKをクリック
+			webDriver.switchTo().alert().accept();
+		} catch (NoAlertPresentException e) {
+			// アラートが出ていなければスルー
+		}
+
+		visibilityTimeout(By.xpath("//h2[text() = '勤怠管理']"), 10);
+
+		assertEquals(webDriver.getTitle(), "勤怠情報変更｜LMS");
+		getEvidence(new Object() {
+		}, "ケース10_No.03_2");
+
 	}
 
 	@Test
 	@Order(4)
 	@DisplayName("テスト04 「出勤」ボタンを押下し出勤時間を登録")
 	void test04() {
-		// TODO ここに追加
+		WebElement punchInButton = webDriver.findElement(By.xpath("//input[@value='出勤']"));
+
+		// テスト実行時刻をHH:mm形式で取得
+		LocalDateTime now = LocalDateTime.now();
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm");
+		String formattedTime = now.format(formatter);
+
+		getEvidence(new Object() {
+		}, "ケース10_No.04_1");
+
+		punchInButton.click();
+
+		try {
+			// アラートが出てきた場合はOKをクリック
+			webDriver.switchTo().alert().accept();
+		} catch (NoAlertPresentException e) {
+			// アラートが出ていなければスルー
+		}
+
+		visibilityTimeout(By.xpath("//h2[text() = '勤怠管理']"), 10);
+
+		// 打刻された出勤時間を取得
+		WebElement attendanceTable = webDriver.findElement(By.xpath("//tr[@class='info']"));
+		WebElement punchInRecord = attendanceTable.findElement(By.xpath("./td[3]"));
+
+		assertEquals(punchInRecord.getText(), formattedTime);
+		getEvidence(new Object() {
+		}, "ケース10_No.04_2");
 	}
 
 	@Test
 	@Order(5)
 	@DisplayName("テスト05 「退勤」ボタンを押下し退勤時間を登録")
 	void test05() {
-		// TODO ここに追加
+		WebElement punchOutButton = webDriver.findElement(By.xpath("//input[@value='退勤']"));
+
+		// テスト実行時刻をHH:mm形式で取得
+		LocalDateTime now = LocalDateTime.now();
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm");
+		String formattedTime = now.format(formatter);
+
+		getEvidence(new Object() {
+		}, "ケース10_No.05_1");
+
+		punchOutButton.click();
+
+		try {
+			// アラートが出てきた場合はOKをクリック
+			webDriver.switchTo().alert().accept();
+		} catch (NoAlertPresentException e) {
+			// アラートが出ていなければスルー
+		}
+
+		visibilityTimeout(By.xpath("//h2[text() = '勤怠管理']"), 10);
+
+		// 打刻された退勤時間を取得
+		WebElement attendanceTable = webDriver.findElement(By.xpath("//tr[@class='info']"));
+		WebElement punchOutRecord = attendanceTable.findElement(By.xpath("./td[4]"));
+
+		assertEquals(punchOutRecord.getText(), formattedTime);
+		getEvidence(new Object() {
+		}, "ケース10_No.05_2");
 	}
 
 }
