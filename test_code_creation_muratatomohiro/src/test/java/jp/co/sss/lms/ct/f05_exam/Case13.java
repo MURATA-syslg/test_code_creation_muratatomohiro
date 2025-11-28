1b988cfe77ed8be164619cbe953acfbf09653c54
t-murata <th.murata@syslg.co.jp>
2025-11-28 16:23:55 +0900

    add:Case13.java 試験実施機能をテストするテストコード追加
    WebElementを利用してログイン後に、用意された試験を実施し、試験実施機能が期待通り動作するかどうか確認するテストを作成
    
    期待値は以下の通り
    試験を受験し、すべて未回答の状態で提出を行い、試験開始画面に戻った際に結果が正しく反映されているかどうか確認

--- test_code_creation_muratatomohiro/src/test/java/jp/co/sss/lms/ct/f05_exam/Case13.java
+++ test_code_creation_muratatomohiro/src/test/java/jp/co/sss/lms/ct/f05_exam/Case13.java
@@ -1,6 +1,7 @@
 package jp.co.sss.lms.ct.f05_exam;
 
 import static jp.co.sss.lms.ct.util.WebDriverUtils.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.Date;
 
@@ -11,6 +12,11 @@ import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoAlertPresentException;
+import org.openqa.selenium.WebElement;
+
+import jp.co.sss.lms.ct.util.WebDriverUtils;
 
 /**
  * 結合テスト 試験実施機能
@@ -40,56 +46,176 @@ public class Case13 {
 	@Order(1)
 	@DisplayName("テスト01 トップページURLでアクセス")
 	void test01() {
-		// TODO ここに追加
+		goTo("http://localhost:8080/lms/");
+
+		getEvidence(new Object() {
+		}, "ケース13_No.01");
+
+		assertEquals(webDriver.getTitle(), "ログイン | LMS");
+		assertEquals(webDriver.findElement(By.className("btn")).getAttribute("value"), "ログイン");
+		assertEquals(webDriver.getCurrentUrl(), "http://localhost:8080/lms/");
 	}
 
 	@Test
 	@Order(2)
 	@DisplayName("テスト02 初回ログイン済みの受講生ユーザーでログイン")
 	void test02() {
-		// TODO ここに追加
+		// ログインに必要な要素の取得
+		WebElement userID = WebDriverUtils.webDriver.findElement(By.id("loginId"));
+		WebElement password = WebDriverUtils.webDriver.findElement(By.id("password"));
+		WebElement loginButton = WebDriverUtils.webDriver.findElement(By.cssSelector("[value='ログイン']"));
+		userID.clear();
+		userID.sendKeys("StudentAA01");
+		password.clear();
+		password.sendKeys("Password01");
+
+		getEvidence(new Object() {
+		}, "ケース13_No.02_1");
+
+		loginButton.click();
+
+		visibilityTimeout(By.className("active"), 10);
+
+		assertEquals(webDriver.getTitle(), "コース詳細 | LMS");
+
+		getEvidence(new Object() {
+		}, "ケース13_No.02_2");
 	}
 
 	@Test
 	@Order(3)
 	@DisplayName("テスト03 「試験有」の研修日の「詳細」ボタンを押下しセクション詳細画面に遷移")
 	void test03() {
-		// TODO ここに追加
+		// 「試験有」の研修日の要素を取得
+		WebElement trainingRecord = webDriver.findElement(By.xpath("//span[contains(text(),'試験有')]/ancestor::tr[1]"));
+		WebElement detailButton = trainingRecord.findElement(By.className("btn"));
+
+		getEvidence(new Object() {
+		}, "ケース13_No.03_1");
+
+		detailButton.click();
+
+		visibilityTimeout(By.className("active"), 10);
+
+		assertEquals(webDriver.getTitle(), "セクション詳細 | LMS");
+		getEvidence(new Object() {
+		}, "ケース13_No.03_2");
 	}
 
 	@Test
 	@Order(4)
 	@DisplayName("テスト04 「本日の試験」エリアの「詳細」ボタンを押下し試験開始画面に遷移")
 	void test04() {
-		// TODO ここに追加
+		WebElement detailButton = webDriver.findElement(By.xpath("//form[@action='/lms/exam/start']"));
+
+		getEvidence(new Object() {
+		}, "ケース13_No.04_1");
+
+		detailButton.click();
+
+		visibilityTimeout(By.className("active"), 10);
+
+		assertTrue(webDriver.getTitle().contains("試験"));
+		getEvidence(new Object() {
+		}, "ケース13_No.04_2");
 	}
 
 	@Test
 	@Order(5)
 	@DisplayName("テスト05 「試験を開始する」ボタンを押下し試験問題画面に遷移")
 	void test05() {
-		// TODO ここに追加
+		WebElement examButton = webDriver.findElement(By.xpath("//input[@value='試験を開始する']"));
+
+		getEvidence(new Object() {
+		}, "ケース13_No.05_1");
+
+		examButton.click();
+
+		visibilityTimeout(By.xpath("//form[@action='/lms/exam/start']"), 10);
+
+		assertEquals(webDriver.findElement(By.id("backForm")).getAttribute("action"),
+				"http://localhost:8080/lms/exam/start");
+		getEvidence(new Object() {
+		}, "ケース13_No.05_2");
 	}
 
 	@Test
 	@Order(6)
 	@DisplayName("テスト06 未回答の状態で「確認画面へ進む」ボタンを押下し試験回答確認画面に遷移")
-	void test06() {
-		// TODO ここに追加
+	void test06() throws InterruptedException {
+		WebElement confirmButton = webDriver.findElement(By.xpath("//input[@value='確認画面へ進む']"));
+
+		scrollByElement(confirmButton);
+
+		getEvidence(new Object() {
+		}, "ケース13_No.06_1");
+
+		// 試験時間確保のため5秒待機
+		Thread.sleep(5000);
+
+		confirmButton.click();
+
+		visibilityTimeout(By.xpath("//form[@action='/lms/exam/result']"), 10);
+
+		assertEquals(webDriver.getCurrentUrl(), "http://localhost:8080/lms/exam/answerCheck");
+		getEvidence(new Object() {
+		}, "ケース13_No.06_2");
 	}
 
 	@Test
 	@Order(7)
 	@DisplayName("テスト07 「回答を送信する」ボタンを押下し試験結果画面に遷移")
 	void test07() throws InterruptedException {
-		// TODO ここに追加
+		WebElement sendButton = webDriver.findElement(By.id("sendButton"));
+
+		scrollByElement(sendButton);
+
+		getEvidence(new Object() {
+		}, "ケース13_No.07_1");
+
+		// 試験時間確保のため5秒待機
+		Thread.sleep(5000);
+		sendButton.click();
+
+		try {
+			// アラートが出てきた場合はOKをクリック
+			webDriver.switchTo().alert().accept();
+		} catch (NoAlertPresentException e) {
+			// アラートが出ていなければスルー
+		}
+
+		visibilityTimeout(By.xpath("//small[contains(text(),'あなたのスコア')]"), 10);
+
+		assertEquals(webDriver.getCurrentUrl(), "http://localhost:8080/lms/exam/result");
+		getEvidence(new Object() {
+		}, "ケース13_No.07_2");
 	}
 
 	@Test
 	@Order(8)
 	@DisplayName("テスト08 「戻る」ボタンを押下し試験開始画面に遷移後当該試験の結果が反映される")
 	void test08() {
-		// TODO ここに追加
+		WebElement backButton = webDriver.findElement(By.xpath("//input[@value='戻る']"));
+
+		scrollByElement(backButton);
+
+		getEvidence(new Object() {
+		}, "ケース13_No.08_1");
+
+		backButton.click();
+
+		try {
+			// アラートが出てきた場合はOKをクリック
+			webDriver.switchTo().alert().accept();
+		} catch (NoAlertPresentException e) {
+			// アラートが出ていなければスルー
+		}
+
+		visibilityTimeout(By.className("active"), 10);
+
+		assertTrue(webDriver.getTitle().contains("試験"));
+		getEvidence(new Object() {
+		}, "ケース13_No.08_2");
 	}
 
 }
